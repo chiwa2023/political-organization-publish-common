3d92e0128c280c0c85b9ef1d33662f1879aac8f0
Chiwaki <chiwa2023@ymail.ne.jp>
2024-08-12 08:55:50 +0900

    add XML全データ用AllBookDto

--- back/publish/src/main/java/mitei/mitei/common/publish/party/usage/report/dto/v5/Shito0801Dto.java
+++ back/publish/src/main/java/mitei/mitei/common/publish/party/usage/report/dto/v5/Shito0801Dto.java
@@ -13,24 +13,24 @@ public class Shito0801Dto implements Serializable {
     /** serialId */
     private static final long serialVersionUID = 1L;
 
-    /** 様式8の3シート */
+    /** 様式8の1シート */
     @JacksonXmlProperty(localName = "SHEET")
     @JacksonXmlElementWrapper(useWrapping = false)
     private Sheet0801Dto sheet0801Dto = new Sheet0801Dto();
 
     /**
-     * 様式8の3シートを取得する
+     * 様式8の1シートを取得する
      *
-     * @return 様式8の3シート
+     * @return 様式8の1シート
      */
     public Sheet0801Dto getSheet0801Dto() {
         return sheet0801Dto;
     }
 
     /**
-     * 様式8の3シートを設定する
+     * 様式8の1シートを設定する
      *
-     * @param sheet0801Dto 様式8の3シート
+     * @param sheet0801Dto 様式8の1シート
      */
     public void setSheet0801Dto(final Sheet0801Dto sheet0801Dto) {
         this.sheet0801Dto = sheet0801Dto;
